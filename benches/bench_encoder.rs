bd8960d3b6800cef739b150cbc2e5015ae0f1b3d
Navoso <VantorreWannes@gmail.com>
2023-12-07 16:40:01 +0100

    Impl CopyInstructionIter benches

--- benches/bench_encoder.rs
+++ benches/bench_encoder.rs
@@ -2,7 +2,6 @@
 
 extern crate test;
 
-
 fn remove_instruction_length(source: &[u8], next_lcs_item: Option<&u8>) -> usize {
     if let Some(&item) = next_lcs_item {
         source.iter().position(|&x| x == item).unwrap_or(0)
@@ -18,7 +17,7 @@ fn add_instruction_length(target: &[u8], next_lcs_item: Option<&u8>) -> usize {
 #[cfg(test)]
 mod encoder_benchmarks {
     use super::*;
-    use fast_deltas::lcs::Lcs;
+    use fast_deltas::{copy_instruction_iterator::CopyInstructionIterator, lcs::Lcs};
     use test::{black_box, Bencher};
 
     #[bench]
@@ -39,7 +38,16 @@ mod encoder_benchmarks {
         b.iter(|| {
             add_instruction_length(black_box(&target), black_box(lcs.first()));
         })
-    }    
+    }
 
-    
+    #[bench]
+    fn bench_copy_instruction_length(b: &mut Bencher) {
+        let mut source = vec![0u8; u16::MAX as usize / 2];
+        source.extend([1; u16::MAX as usize / 2]);
+        let target = [0; u16::MAX as usize];
+        let lcs = [0; u16::MAX as usize / 2];
+        b.iter(|| {
+            CopyInstructionIterator::new(black_box(&lcs), black_box(&target), black_box(&target));
+        })
+    }
 }
