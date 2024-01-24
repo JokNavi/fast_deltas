828076b53ea1d98e02c4674f0d108451ce940ea9
Navoso <VantorreWannes@gmail.com>
2024-01-24 18:54:13 +0100

    Add benchmarks

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,6 +1,28 @@
 pub mod lcs;
+use std::{fs::OpenOptions, io};
+
+use divan::{Bencher, black_box};
+use fast_deltas::encoder::delta_encode;
 
 fn main() {
     divan::main();
+}
+
+#[divan::bench]
+fn bench_delta_encode(bencher: Bencher){
+    let source = OpenOptions::new()
+            .read(true)
+            .open("test_files/source.txt").unwrap();
+        let target = OpenOptions::new()
+            .read(true)
+            .open("test_files/target.txt").unwrap();
+        let patch = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open("test_files/patch.dpatch").unwrap();
+    bencher.bench_local(move || {
+        black_box(delta_encode(&source, &target, &patch)).unwrap();
+    });
 }
 
